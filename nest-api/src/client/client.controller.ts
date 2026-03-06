89e25e585daaae74285d8ce5a48e7844d4b88856
Mikelombamba <mike.lombamba-yenge@student.junia.com>
2026-03-06 17:20:19 +0100

    UI: Ajout des avatars et de la barre de recherche clients

--- nest-api/src/client/client.controller.ts
+++ nest-api/src/client/client.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { Client } from './client.entity';
+import { Patch } from '@nestjs/common'; // N'oublie pas l'import !
+
 
 @Controller('clients')
 export class ClientController {
@@ -15,4 +17,13 @@ export class ClientController {
   create(@Body() clientData: any) {
     return this.clientService.create(clientData);
   }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+  return this.clientService.remove(+id); // Le + transforme le texte en nombre
+}
+@Patch(':id')
+update(@Param('id') id: string, @Body() data: Partial<Client>) {
+  return this.clientService.update(+id, data);
+}
+
 }
--- nest-api/src/client/client.service.ts
+++ nest-api/src/client/client.service.ts
@@ -18,4 +18,16 @@ export class ClientService {
     const newClient = this.clientRepository.create(clientData);
     return this.clientRepository.save(newClient);
   }
+  async remove(id: number): Promise<void> {
+    await this.clientRepository.delete(id);
+  }
+
+  async update(id: number, data: Partial<Client>): Promise<Client> {
+    await this.clientRepository.update(id, data);
+    const updatedClient = await this.clientRepository.findOneBy({ id });
+    if (!updatedClient) {
+        throw new Error(`Client avec l'id ${id} non trouvé`);
+    }
+    return updatedClient;
+}
 }
